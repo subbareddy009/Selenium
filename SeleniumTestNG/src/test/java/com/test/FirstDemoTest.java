9a7c1af3e9910c7893369cac0c9ca69d00265d20
Training_C2a.05.01 <Training_C2a.05.01@CDC2-D-CJ1QXJ2.dir.svc.accenture.com>
2019-04-02 17:49:29 +0530

    commit2

--- SeleniumTestNG/src/test/java/com/test/FirstDemoTest.java
+++ SeleniumTestNG/src/test/java/com/test/FirstDemoTest.java
@@ -1,5 +1,7 @@
 package com.test;
-
+/**
+ * @author Training_C2a.05.01
+ */
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
